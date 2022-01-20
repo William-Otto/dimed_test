22b694d81419ddcf3a219ccdcb68ba0247cdae39
William Otto <williamroberto321@gmail.com>
2022-01-20 18:56:15 -0300

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,14 @@
 <template>
-  <ApiData/>
+  <Header />
 </template>
 
 <script>
-import ApiData from './components/ApiData.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    ApiData
+    Header
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
