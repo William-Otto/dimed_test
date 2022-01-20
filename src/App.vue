9a05fe798c235e606833145e20f8d65a9bb2405f
William Otto <williamroberto321@gmail.com>
2022-01-20 15:55:58 -0300

    let's rock it

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ApiData/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ApiData from './components/ApiData.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ApiData
   }
 }
 </script>
