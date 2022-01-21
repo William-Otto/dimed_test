be07edea3ee2adbdedb6a40a6eb658a17837cb43
William Otto <williamroberto321@gmail.com>
2022-01-21 14:29:24 -0300

    add modal to search linhas de onibus

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,22 +8,22 @@
                 <h1 class="display-5 fw-bold lh-1 mb-3">Encontre a linha e horário</h1>
                 <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
                 <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                <button data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                <button data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" class="btn btn-primary btn-lg px-4 me-md-2">Onibus</button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Lotação</button>
                 </div>
             </div>
-            <ModalBuses />
+            <Modal />
         </div>
     </div>
 </template>
 
 <script>
-import ModalBuses from './ModalBuses.vue'
+import Modal from './Modal.vue'
 
 export default {
     name: 'Main',
     components: {
-        ModalBuses
+        Modal
     }
 }
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-scrollable">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Linhas de Ônibus</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <input v-model="search" class="form-control mb-3" list="datalistOptions" id="exampleDataList" placeholder="Pesquise pelo nome">
+                <datalist id="datalistOptions">
+
+                </datalist>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Linha</th>
+                            <th scope="col">Tipo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="bus in filteredBuses" :key="bus._id">
+                            <td>{{ bus.nome }}</td>
+                            <td>{{ bus.linha }}</td>
+                            <td>{{ bus.tipo }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <nav aria-label="...">
+                    <ul class="pagination">
+                        <li class="page-item">
+                            <a class="page-link" @click="previousPage()">Previous</a>
+                        </li>
+                        <li class="page-item">
+                            <a class="page-link" @click="nextPage()">Next</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { getFirstPage, getPage } from '../services/bus'
+
+export default {
+    name: 'Modal',
+    data() {
+        return {
+            buses: [],
+            search: '',
+            next: '',
+            prev: '',
+        }
+    },
+    computed: {
+        filteredBuses() {
+            return this.buses.filter((bus) => {
+                return bus.nome.match(this.search.toUpperCase())
+            })
+        },
+    },
+    methods: {
+        nextPage() {
+            getPage(this.next).then((res) => {
+                this.buses = res.data.result.records
+                this.next = res.data.result._links.next
+                if (res.data.result._links.prev) {
+                    this.prev = res.data.result._links.prev
+                }
+            })
+        },
+        previousPage() {
+            getPage(this.prev).then((res) => {
+                this.buses = res.data.result.records
+                this.next = res.data.result._links.next
+                if (res.data.result._links.prev) {
+                    this.prev = res.data.result._links.prev
+                }
+            })
+        },
+    },
+    created() {
+        getFirstPage().then((res) => {
+            this.buses = res.data.result.records
+            this.next = res.data.result._links.next
+        })
+    }
+}
+</script>
